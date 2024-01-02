4343cd7749368a3560f5279530d8443b3da37fca
Benjamin J. Young <ben@blaher.me>
2024-01-02 11:18:30 -0500

    Updating PHP

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /**
  * Autoshop Template Configuration
  *
- * PHP version 8.1
+ * PHP version 8.2
  *
- * @phpversion >= 8.1
+ * @phpversion >= 8.2
  * @category   CMS
  * @package    AVCorn
  * @subpackage Template
