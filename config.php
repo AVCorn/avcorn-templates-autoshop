5660720e10b7c5597f2cf5c0494602def173e928
Benjamin J. Young <ben@blaher.me>
2024-01-08 08:36:28 -0500

    Formatting config

--- config.php
+++ config.php
@@ -50,6 +50,6 @@ $config['navigation'] = [
 ];
 
 $config['info']['company'] = 'Nuthead Auto Shop';
-$config['info']['title'] = 'Nuthead Auto Shop';
-$config['info']['motto'] = 'The Craziest Auto Shop Around!';
-$config['info']['slogan'] = 'The Craziest Auto Shop Around!';
+$config['info']['title']   = 'Nuthead Auto Shop';
+$config['info']['motto']   = 'The Craziest Auto Shop Around!';
+$config['info']['slogan']  = 'The Craziest Auto Shop Around!';
